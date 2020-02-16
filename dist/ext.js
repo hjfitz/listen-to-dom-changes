00d6b8919b734ce00d2897d4989797f5b922f2f7
harry fitzgerald <hfitz83@gmail.com>
2020-02-16 16:24:52 +0000

    init

---
+++ dist/ext.js
@@ -0,0 +1,27 @@
+// const p = document.createElement('p')
+// p.textContent = 'Extension loaded'
+
+document.addEventListener('DOMContentLoaded', () => {
+	// document.body.appendChild(p)
+	const audioCtx = new(window.AudioContext || window.webkitAudioContext)()
+	const observer = new MutationObserver(function (mutationsList) {
+		const oscillator = audioCtx.createOscillator()
+
+		oscillator.connect(audioCtx.destination)
+		oscillator.type = "sine"
+		oscillator.frequency.setValueAtTime(
+			Math.log(mutationsList.length + 5) * 880,
+			audioCtx.currentTime,
+		)
+
+		oscillator.start()
+		oscillator.stop(audioCtx.currentTime + 0.01)
+	})
+
+	observer.observe(document, {
+		attributes: true,
+		childList: true,
+		subtree: true,
+		characterData: true,
+	})
+})
